47c76849ca24615e166967cf9c6cd3c485818fe0
luke-h1 <luke.howsam@yahoo.com>
2020-10-22 20:18:15 +0100

    ts results

--- typescript/src/Results.tsx
+++ typescript/src/Results.tsx
@@ -1,15 +1,32 @@
 import React from "react";
-import pf from "petfinder-client";
+import pf, { Pet as PetType } from "petfinder-client";
+import { RouteComponentType, RouteComponentProps } from "@reach/router";
 import Pet from "./Pet";
 import SearchBox from "./SearchBox";
 import { Consumer } from "./SearchContext";
+
+if (!process.env.API_KEY || !process.env.API_SECRET) {
+  throw new Error("No API keys");
+}
+
+interface Props {
+  searchParams: {
+    location: string;
+    animal: string;
+    breed: string;
+  };
+}
+
+interface State {
+  pets: PetType[];
+}
 
 const petfinder = pf({
   key: process.env.API_KEY,
   secret: process.env.API_SECRET
 });
 
-class Results extends React.Component {
+class Results extends React.Component<Props, State> {
   constructor(props) {
     super(props);
 
@@ -17,10 +34,10 @@ class Results extends React.Component {
       pets: []
     };
   }
-  componentDidMount() {
+  public componentDidMount() {
     this.search();
   }
-  search = () => {
+  public search = () => {
     petfinder.pet
       .find({
         location: this.props.searchParams.location,
@@ -29,7 +46,7 @@ class Results extends React.Component {
         output: "full"
       })
       .then(data => {
-        let pets;
+        let pets: PetType;
         if (data.petfinder.pets && data.petfinder.pets.pet) {
           if (Array.isArray(data.petfinder.pets.pet)) {
             pets = data.petfinder.pets.pet;
@@ -40,11 +57,11 @@ class Results extends React.Component {
           pets = [];
         }
         this.setState({
-          pets: pets
+          pets
         });
       });
   };
-  render() {
+  public render() {
     return (
       <div className="search">
         <SearchBox search={this.search} />
@@ -72,7 +89,7 @@ class Results extends React.Component {
   }
 }
 
-export default function ResultsWithContext(props) {
+export default function ResultsWithContext(props: RouteComponentProps) {
   return (
     <Consumer>
       {context => <Results {...props} searchParams={context} />}
